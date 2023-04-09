eed9587a0abcbc3d3efb8b505bff8b5685b56ad8
Wojciech Ciezobka <wciezobka@student.agh.edu.pl>
2023-04-09 18:09:29 +0200

    evaluation metrices

--- models/XGB/forecast.py
+++ models/XGB/forecast.py
@@ -52,38 +52,6 @@ def get_forecast_series_ar(forecast: npt.NDArray, train_ds_size: int, test_ds_si
     return pd.Series(forecast, index=range(train_ds_size, train_ds_size + test_ds_size))
 
 
-def plot_forecasts(dataset_series: pd.Series, forecast_series: List[pd.Series], xlim: Tuple[int, int]):
-
-    plt.scatter(dataset_series.index, dataset_series.values, color='tab:blue', label='Original', s=2)
-
-    for series in forecast_series:
-        plt.scatter(series.index, series.values, color="tab:orange", s=2, alpha=0.1)
-
-    plt.scatter([-10], [0], color="tab:orange", label='Prediction', s=2, alpha=0.1)
-    plt.legend()
-    plt.xlabel("Month index")
-    plt.ylabel("Monthly Mean Total Sunspot Number")
-    plt.title("Sumaric forecast results")
-    plt.xlim(xlim)
-    plt.show()
-
-
-def plot_forecast(dataset_series: pd.Series, forecast_series: List[pd.Series], idx: int, xlim: Tuple[int, int]):
-    
-    suffix = defaultdict(lambda: "th", {1: "st", 2: "nd", 3: "rd"})
-
-
-    plt.scatter(dataset_series.index, dataset_series.values, color='tab:blue', label='Original', s=2)
-    plt.scatter(forecast_series[idx].index, forecast_series[idx].values, color="tab:orange", label='Prediction', s=2)
-
-    plt.legend()
-    plt.xlabel("Month index")
-    plt.ylabel("Monthly Mean Total Sunspot Number")
-    plt.title(f"Forecast since {idx + 1}{suffix[idx + 1]} data patch")
-    plt.xlim(xlim)
-    plt.show()
-
-
 def get_RMSE(y: pd.Series, y_hat: pd.Series, window: int = 10) -> Tuple[str, float, pd.Series]:
     
     rmse = np.sqrt(np.mean((y_hat - y)**2))
@@ -114,6 +82,78 @@ METRICES_MAP = {
 }
 
 
+def plot_forecasts(
+    dataset_series: pd.Series,
+    forecast_series: List[pd.Series],
+    xlim: Tuple[int, int],
+    metric_name: str = "RMSE"
+):
+
+    metric_func = METRICES_MAP[metric_name.upper()]
+
+    # Scatter original data
+    plt.scatter(dataset_series.index, dataset_series.values, color='tab:blue', label='Original', s=2)
+
+    metric_vals = list()
+    for series in forecast_series:
+
+        # Calculate metrices
+        metric_name, metric, metric_series = metric_func(y=dataset_series[series.index], y_hat=series)
+        metric_vals.append(metric)
+        
+        # Scatter forecast
+        plt.scatter(series.index, series.values, color="tab:orange", s=2, alpha=0.1)
+
+        # Plot evaluation metric
+        plt.plot(metric_series.index, metric_series.values, color="red", alpha=0.1, lw=1)
+    
+    metric_aggregated = np.mean(np.array(metric_vals))
+
+    # Legend hack
+    plt.scatter([-10], [0], color="tab:orange", label='Prediction', s=2)
+    plt.plot([-10, -9], [0, 0], color="red", label=metric_name, alpha=0.5, lw=1)
+
+    plt.legend()
+    plt.xlabel("Month index")
+    plt.ylabel("Monthly Mean Total Sunspot Number")
+    plt.title(f"Sumaric forecast results\n{metric_name}: {round(metric_aggregated, 3)}")
+    plt.xlim(xlim)
+    plt.ylim(top=dataset_series.max()*1.2)
+    plt.show()
+
+
+def plot_forecast(
+    dataset_series: pd.Series,
+    forecast_series: List[pd.Series],
+    idx: int,
+    xlim: Tuple[int, int],
+    metric_name: str = "RMSE"
+):
+    
+    suffix = defaultdict(lambda: "th", {1: "st", 2: "nd", 3: "rd"})
+
+    # Calculate metrices
+    metric_func = METRICES_MAP[metric_name.upper()]
+    metric_name, metric, metric_series = metric_func(y=dataset_series[forecast_series[idx].index], y_hat=forecast_series[idx])
+
+    # Scatter original data
+    plt.scatter(dataset_series.index, dataset_series.values, color='tab:blue', label='Original', s=2)
+
+    # Scatter forecast
+    plt.scatter(forecast_series[idx].index, forecast_series[idx].values, color="tab:orange", label='Prediction', s=2)
+
+    # Plot evaluation metric
+    plt.plot(metric_series.index, metric_series.values, color="red", label=metric_name, alpha=0.5)
+
+    plt.legend()
+    plt.xlabel("Month index")
+    plt.ylabel("Monthly Mean Total Sunspot Number")
+    plt.title(f"Forecast since {idx + 1}{suffix[idx + 1]} data patch\n{metric_name}: {round(metric, 3)}")
+    plt.xlim(xlim)
+    plt.ylim(top=dataset_series.max()*1.2)
+    plt.show()
+
+
 def plot_forecast_ar(
     dataset_series: pd.Series,
     forecast_series: pd.Series,
@@ -132,7 +172,7 @@ def plot_forecast_ar(
     # Scatter forecast
     plt.scatter(forecast_series.index, forecast_series.values, color="tab:orange", label='Prediction', s=2)
 
-    # Scatter evaluation metric
+    # Plot evaluation metric
     plt.plot(metric_series.index, metric_series.values, color="red", label=metric_name, alpha=0.5)
 
     plt.legend()
@@ -140,5 +180,5 @@ def plot_forecast_ar(
     plt.ylabel("Monthly Mean Total Sunspot Number")
     plt.title(f"Autoregressive forecast for {n_steps_in} values lookbehind\n{metric_name}: {round(metric, 3)}")
     plt.xlim(xlim)
-    plt.ylim(top=dataset_series.max()*1.4)
+    plt.ylim(top=dataset_series.max()*1.2)
     plt.show()
